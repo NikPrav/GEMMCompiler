856aa0a74a423afa1cb26e48886e56b6e2b00464
NikPrav <nikhil.pravin@gmail.com>
2024-04-25 23:42:02 -0400

    removed extra files

--- bitfiles/program.sh
+++
@@ -1,2 +0,0 @@
-BITFILE=$1
-vivado -mode batch -notrace -nojournal -log ./log/vivado.log -source program.tcl -tclargs --bitfile $BITFILE
--- driver/README.md
+++
@@ -1,2 +0,0 @@
-To install drivers, please follow the link here:
-  www.xilinx.com/support/answers/65444.html
